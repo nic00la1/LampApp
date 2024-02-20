4e823cde2d523afb5fc4e0c6263339e0ce1d90fc
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-20 21:27:10 +0100

    LightShape ✅

---
+++ Views/LightShape.swift
@@ -0,0 +1,23 @@
+//
+//  LightShape.swift
+//  LampApp
+//
+//  Created by Nicola Kaleta on 20/02/2024.
+//
+
+import SwiftUI
+
+// MARK: - Represent the shape of the light emmited by the lamp
+struct LightShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path {
+            path in
+            path.addLines([
+                .init(x: rect.width * 0.1, y: 0),
+                .init(x: rect.width * 0.7, y: 0),
+                .init(x: rect.width, y: rect.height * 0.6),
+                .init(x: 0, y: rect.height * 0.6),
+            ])
+        }
+    }
+}

ba1aaaa2c18d48a4d325e4dc6a6436835a439ef7
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-20 21:20:29 +0100

    Initial commit

---
+++ LampAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampAppApp.swift
+//  LampApp
+//
+//  Created by Nicola Kaleta on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LampAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

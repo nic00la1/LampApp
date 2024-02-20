a7f4f61c4b40166be669eff1261b5ab88f691cf3
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-20 21:49:43 +0100

    opacitydisplay

---
+++ Views/OpacityDisplay.swift
@@ -0,0 +1,24 @@
+//
+//  OpacityDisplay.swift
+//  LampApp
+//
+//  Created by Nicola Kaleta on 20/02/2024.
+//
+
+import SwiftUI
+
+// MARK: - Display the current Opacity Value
+struct OpacityDisplay: View {
+    @Binding var opacity: Double
+    var body: some View {
+        Text("\(String(format: "%.2f", opacity))")
+            .foregroundStyle(.white)
+            .font(.system(size: 80))
+            .opacity(opacity + 0.4)
+            .offset(x: -60)
+    }
+}
+
+#Preview {
+    OpacityDisplay(opacity: .constant(0.1))
+}

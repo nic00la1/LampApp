ac8a3236f76344e8c40be06bbe3ba72df03d7d79
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-20 21:37:51 +0100

    LightEffect

---
+++ Views/LightEffect.swift
@@ -0,0 +1,24 @@
+//
+//  LightEffect.swift
+//  LampApp
+//
+//  Created by Nicola Kaleta on 20/02/2024.
+//
+
+import SwiftUI
+// MARK: - Effect of the light
+struct LightEffect: View {
+    @Binding var opacity: Double
+    var body: some View {
+        LightShape()
+            .fill(LinearGradient(gradient: Gradient(colors: [.white, .clear, .clear]), startPoint: .top, endPoint: .bottom))
+            .frame(width: 300, height: 600)
+            .foregroundStyle(.blue)
+            .offset(x: -35, y: 250)
+            .opacity(opacity)
+    }
+}
+
+#Preview {
+    LightEffect(opacity: .constant(0.1))
+}
